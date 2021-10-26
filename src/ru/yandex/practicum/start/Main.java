478b7095fd2b199e12427a1fe4c9e01a42e61e6d
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-26 23:09:29 +0300

    modified(Main.java): Modified variables

--- src/ru/yandex/practicum/start/Main.java
+++ src/ru/yandex/practicum/start/Main.java
@@ -3,6 +3,12 @@ package ru.yandex.practicum.start;
 public class Main {
 
     public static void main(String[] args) {
+        // Вывод в консоль строками: 'println' -выводит построчно
+        // Тогда как 'print' будет выводить одной строкой
         System.out.println("Теперь работает!");
+        // создание переменных
+        int name = 10;
+        double pi = 3.14;
+        String mint = "mint";
     }
 }
