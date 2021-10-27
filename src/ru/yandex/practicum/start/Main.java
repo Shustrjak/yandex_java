798f9b5cc654b5e1331639929ab54104342c97ba
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-28 01:07:51 +0300

    recreated project

--- src/ru/yandex/practicum/start/Main.java
+++ src/ru/yandex/practicum/start/Main.java
@@ -3,10 +3,6 @@ package ru.yandex.practicum.start;
 public class Main {
 
     public static void main(String[] args) {
-        double rubles = 30000.0;
-        double rateUSD = 78.5;
-        double dollar = (rubles / rateUSD);
-        String dollars = String.format("%.2f", dollar);
-        System.out.println("У вас на счету " + rubles + " рублей. В долларах это " + dollars + " долларов. Так держать!");
+	// write your code here
     }
 }
