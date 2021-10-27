e6b7adcb4ae33e6cb14bff164f7fa968a16cae92
Alexander Rudenko <rudenko.alex.ol@gmail.com>
2021-10-27 21:43:49 +0300

    modified(Main.java) UPD check code on write how to reduce the number of characters after the dot

--- src/ru/yandex/practicum/start/Main.java
+++ src/ru/yandex/practicum/start/Main.java
@@ -3,13 +3,10 @@ package ru.yandex.practicum.start;
 public class Main {
 
     public static void main(String[] args) {
-        // Вывод в консоль строками: 'println' -выводит построчно
-        // Тогда как 'print' будет выводить одной строкой
-        System.out.println("Теперь работает!");
-        // создание переменных
-        int name = 10;
-        double pi = 3.14;
-        String mint = "mint";
-        System.out.println(name + pi + mint);
+        double rubles = 30000.0;
+        double rateUSD = 78.5;
+        double dollar = (rubles / rateUSD);
+        String dollars = String.format("%.2f", dollar);
+        System.out.println("У вас на счету " + rubles + " рублей. В долларах это " + dollars + " долларов. Так держать!");
     }
 }
